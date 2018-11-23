ed8fddd75faaee9a44fb8ba8cb0ddc66366bb95c
Jason <Teamtwose12094@gmail.com>
2018-11-22 17:52:34 -0700

    changed gear ratio and built the blue and red crater autonomous programs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedCrater.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedCrater.java
@@ -8,14 +8,15 @@ package org.firstinspires.ftc.teamcode;
 public class RedCrater extends Autonomous {
     @Override
     public void runPath() {
-        move (25, 0.7);
+        move (10, 0.9);
         //knockem over
-        pivot(-100,0.7);
-        move(50, 0.7);
-        pivot(-80, 0.7);
-        move (50, 0.7);
-        pivot(-180,0.7);
-        move(50,0.7);
+        pivot(-100,0.9);
+        move(10, 0.9);
+        pivot(-80, 0.9);
+        move (10, 0.9);
+        pivot(-180,0.9);
+        move(10,0.9);
+
 
     }
 }
