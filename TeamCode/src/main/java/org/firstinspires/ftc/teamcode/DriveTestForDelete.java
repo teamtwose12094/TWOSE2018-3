a68ac6c0b23cc74043d13d85b31cf64ace9c00b5
Alex <Teamtwose12094@gmail.com>
2019-02-28 19:45:22 -0700

    Merge branch 'master' of C:\Users\Instructor\Documents\FTC Programming Resources\TWOSE2018-3-2 with conflicts.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTestForDelete.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by student on 1/20/2017.
+ */
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "MissionCornerBlue", group = "Autonomous")
+public class DriveTestForDelete extends Autonomous {
+    @Override
+    public void runPath() {
+        move(10, 0.7);
+    }}
